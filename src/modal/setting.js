08fe2c0da736e1ee23a232c02fbda1a39f62e6aa
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2018-11-27 14:53:31 +0900

    setting page modi

--- src/modal/setting.js
+++ src/modal/setting.js
@@ -44,19 +44,17 @@ export default class App extends Component {
             <View style={styles.container}>
                 <View style={{ justifyContent: 'center', alignItems: 'center', width: this.state.window.width, height: this.state.window.width / 2 }}>
                     <ImageBackground source={{uri:this.state.recent.image}} style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
-                        <View style={{ borderRadius: 100, borderWidth: 15, borderColor: 'rgba(255,255,255, 0.5)', width: this.state.window.width / 2 - 20, height: this.state.window.width / 2 - 20, justifyContent: 'center' }}>
-                            <Image source={require('../../images/ddobuck.png')} style={{ flex: 1, alignSelf: 'stretch', borderColor: 'white', borderWidth: 5, borderRadius: 100, width: null }}></Image>
-                        </View>
+                        <TouchableOpacity onPress={() => {this.props.change("mypage");}}>
+                            <View style={{ borderRadius: 100, borderWidth: 15, borderColor: 'rgba(255,255,255, 0.5)', width: this.state.window.width / 2 - 20, height: this.state.window.width / 2 - 20, justifyContent: 'center' }}>
+                                <Image source={require('../../images/ddobuck.png')} style={{ flex: 1, alignSelf: 'stretch', borderColor: 'white', borderWidth: 5, borderRadius: 100, width: null }}></Image>
+                            </View>
+                        </TouchableOpacity>
                     </ImageBackground>
                 </View>
 
-                <TouchableOpacity onPress={() => {this.props.change("mypage");}}>
                     <View style={styles.settingview1}>
-                        <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#00AFFF', height: 40, justifyContent: 'center'}}>
-                            <Text style={{fontSize: 20, color: 'white', fontWeight: 'bold' }}>마이페이지로 이동</Text>
-                        </View>
+                        <Text style={{fontSize: 17, color: 'white', fontWeight: 'bold',}}>여행자님이 방문한 Travel Stop</Text>
                     </View>
-                </TouchableOpacity>
 
                 <ScrollView>
                     <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
@@ -78,7 +76,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: '#4dc3ff',
+        backgroundColor: '#F2F2F2',
     },
     view: {
         flex: 1
@@ -86,9 +84,8 @@ const styles = StyleSheet.create({
     settingview1: {
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection: 'row',
         height: 40,
-        marginBottom: 1.5,
+        backgroundColor: '#00afff'
     },
     settingview3: {
         backgroundColor: 'white',
