1d85d4c0d0363650fcbed2ada2a9633809beca0d
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2018-11-30 21:02:45 +0900

    ui modi

--- src/travelStop.js
+++ src/travelStop.js
@@ -16,8 +16,8 @@ export default class App extends Component {
         const { width, height } = Dimensions.get("window")
         this.state = {
             _id : props.travelStopId,
-            title : "두리두리 용두리",
-            image : 'http://www.doopedia.co.kr/_upload/image4/1711/03/171103021618274/171103021618274_thumb_400.jpg',
+            title : "",
+            image : "",
             description : "",
             comment : [],
             window: {
@@ -136,8 +136,10 @@ export default class App extends Component {
         )
 
         T_Info = (
-            <View style={styles.T_Info}>
-                <Text style={styles.T_Info_Text}>{this.state.description ? this.state.description : "Description이 작성되지 않은 TravelStop 입니다."}</Text>
+            <View style={[styles.T_Info, ]}>
+                <View style={[styles.info_Box, {width: this.state.window.width - 60}]}>
+                    <Text style={styles.T_Info_Text}>{this.state.description ? this.state.description : "Description이 작성되지 않은 TravelStop 입니다."}</Text>
+                </View>
             </View>
         )
 
@@ -146,8 +148,8 @@ export default class App extends Component {
 
             this.state.comment.forEach(comment => {
                 mentions.push((
-                    <View style={styles.Mention_Group1}>
-                        <Text style={styles.T_Mention_Text1}>
+                    <View style={styles.Mention_Group}>
+                        <Text style={styles.T_Mention_Text}>
                             {comment}
                         </Text>
                     </View>
@@ -231,11 +233,11 @@ export default class App extends Component {
 
                         {T_Mention}
 
+                    </ScrollView>
                         {T_Map}
 
                         {T_ButtonGroup}
 
-                    </ScrollView>
                 </View>
             </View>
         );
@@ -247,6 +249,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
+        backgroundColor: 'white'
     },
 
     T_Title: {
@@ -278,15 +281,24 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         alignContent: 'center',
         alignSelf: 'stretch',
-        padding: 10,
-        paddingLeft: 40,
-        paddingRight: 40,
+        padding: 30,
+    },
+
+    info_Box: {
+        backgroundColor: '#FAFAFA',
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#A4A4A4',
     },
 
     T_Info_Text: {
         fontSize: 12,
         color: '#000000',
-        lineHeight: 15,
+        lineHeight: 17,
     },
 
     T_Mention: {
@@ -294,16 +306,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'stretch',
+        paddingBottom: 15,
     },
 
-    T_Mention_Text1: {
+    T_Mention_Text: {
         fontSize: 10,
         color: '#00AFFF',
-    },
-
-    T_Mention_Text2: {
-        fontSize: 10,
-        color: '#FF0000',
     },
 
     T_Map: {
@@ -339,7 +347,7 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
     },
 
-    Mention_Group1: {
+    Mention_Group: {
         height: 25,
         justifyContent: 'center',
         borderRadius: 7,
@@ -353,22 +361,6 @@ const styles = StyleSheet.create({
         marginLeft: 30,
         marginRight: 30,
     },
-
-    Mention_Group2: {
-        height: 25,
-        justifyContent: 'center',
-        borderRadius: 7,
-        borderWidth: 1,
-        backgroundColor: '#FFFFFF',
-        borderColor: '#00AFFF',
-        marginBottom: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        alignSelf: 'stretch',
-        marginLeft: 30,
-        marginRight: 30,
-    },
-
 
     mapview:{
       height: 160,
